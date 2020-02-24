9e0ff991e2a65517147050302595b4609a450b25
marcelm <mpernia@riservi.com>
2020-02-24 08:51:28 -0500

    se genera formulario

--- contact/views.py
+++ contact/views.py
@@ -4,4 +4,7 @@ from .forms import ContactForm
 # Create your views here.
 def contact(request):
 	contact_form = ContactForm()
+	if request.method == 'POST':
+		contact_form = ContactForm(data=request.POST)
+
 	return render(request, 'contact/contact.html', {'form':contact_form})
